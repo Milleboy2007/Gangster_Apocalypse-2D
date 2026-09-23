9ebf2aa7a3714066628987f53d096e92effab463
Milleboy2007 <2484435@cegepmv.ca>
2026-09-23 15:15:07 -0400

    Animation Player

--- Assets/Script/MovePlayer.cs
+++ Assets/Script/MovePlayer.cs
@@ -7,10 +7,12 @@ public class MovePlayer : MonoBehaviour
 
     private Rigidbody2D corps;
     private Vector2 direction;
+    private Animator animator;
 
     private void Awake()
     {
         corps = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -18,6 +20,15 @@ public class MovePlayer : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         direction = new Vector2(horizontal, vertical).normalized;
+
+        animator.SetFloat("Vitesse", direction.sqrMagnitude);
+
+        if (direction[0] > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }else if(direction[0] < 0){
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 
     private void FixedUpdate()
